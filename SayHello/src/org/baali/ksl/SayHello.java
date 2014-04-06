4fbd962e4be7659e9d036b5b55ce2f82b0802d7d
balaji <balaji@Rajan>
2014-04-06 15:57:24 +0530

    Initial Commit

---
+++ SayHello/src/org/baali/ksl/SayHello.java
@@ -0,0 +1,11 @@
+package org.baali.ksl;
+
+public class SayHello
+{
+
+	public SayHello(String name)
+	{
+		System.out.println("Hello " + name);
+	}
+
+}
---
+++ SayHello/src/org/baali/ksl/test/SayHelloTest.java
@@ -0,0 +1,14 @@
+package org.baali.ksl.test;
+
+import org.baali.ksl.SayHello;
+
+public class SayHelloTest
+{
+
+	public static void main(String[] args)
+	{
+		SayHello  hello = new SayHello("Balaji .M .....");
+
+	}
+
+}
